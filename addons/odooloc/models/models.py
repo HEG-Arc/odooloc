155052ab78ac54c29efce2441755bf6ab9277e0a
fheche <francis.heche@gmail.com>
2019-05-23 18:15:18 +0200

    Adding fk field for reference product into equipment model

--- addons/odooloc/models/models.py
+++ addons/odooloc/models/models.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 class odooloc(models.Model):
     _name = 'odooloc.odooloc'
 
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
+
+
+class MaintenanceEquipment(models.Model):
+    _inherit = "maintenance.equipment"
+
+    odooloc_reference_product_id = fields.Many2one('reference.product', string='Reference product')
