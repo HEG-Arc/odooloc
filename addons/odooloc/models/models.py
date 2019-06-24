dad65001ad8528aa34bb9037de19d8c6a180a119
fheche <francis.heche@gmail.com>
2019-06-24 16:30:48 +0200

    Creating skeleton for rental model and view

--- addons/odooloc/models/models.py
+++ addons/odooloc/models/models.py
@@ -3,13 +3,17 @@ from odoo import models, fields, api
 
 
 class odooloc(models.Model):
-    _name = 'odooloc.odooloc'
-
-    name = fields.Char(string="Title", required=True)
-    description = fields.Text()
+    _name = 'odooloc.rental'
+    # _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Rental"
+    _order = "name"
+    name = fields.Char('Name', index=True, required=True, translate=True)
+    sequence = fields.Integer('Sequence', default=1, help='Gives the sequence order when displaying a rental order list')
+    customer_id = fields.Many2one('res.partner', 'Customer', required=True, domain="[('customer', '=', 'true')]")
+    barcode_id = fields.Many2one('res.partner', 'Customer', domain="[('customer', '=', 'true')]")
 
 
 class MaintenanceEquipment(models.Model):
     _inherit = "maintenance.equipment"
 
-    odooloc_reference_product_id = fields.Many2one('product.template', string='Reference product', domain="[('rental', '=', 1)]")
+    odooloc_reference_product_id = fields.Many2one('stock.production.lot', string='Reference product', domain="[('name', '>', 0)]")
