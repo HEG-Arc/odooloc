44dc87bb08445311cfff36dfa20b51dff016009a
fheche <francis.heche@gmail.com>
2019-08-28 17:37:47 +0200

    Replace inherited methods by custom

--- addons/odooloc/models/odooloc.py
+++ addons/odooloc/models/odooloc.py
@@ -156,7 +156,7 @@ class odoolocOrder(models.Model):
         '''
                 This function opens a window to compose an email, with the edi sale template message loaded by default
                 '''
-        # self.ensure_one()
+        self.ensure_one()
         # ir_model_data = self.env['ir.model.data']
         # try:
         #     template_id = ir_model_data.get_object_reference('odooloc', 'email_template_edi_odooloc')[1]
@@ -198,8 +198,6 @@ class odoolocOrder(models.Model):
 
     @api.multi
     def _odooloc_confirm_order(self):
-        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-            order.message_subscribe([order.partner_id.id])
         self.write({
             'state': 'rent',
             'confirmation_date': fields.Datetime.now()
@@ -251,8 +249,6 @@ class odoolocOrderLine(models.Model):
     _description = "Rental order line"
     _order = 'order_id desc, name desc'
 
-
-
     @api.depends('product_uom_qty', 'discount', 'rental_price', 'tax_id')
     def _compute_amount(self):
         """
@@ -268,57 +264,14 @@ class odoolocOrderLine(models.Model):
                 'price_subtotal': taxes['total_excluded'],
             })
 
-    @api.depends('rental_price', 'discount')
-    def _get_price_reduce(self):
-        for line in self:
-            line.price_reduce = line.rental_price * (1.0 - line.discount / 100.0)
-
-    @api.depends('price_total', 'product_uom_qty')
-    def _get_price_reduce_tax(self):
-        for line in self:
-            line.price_reduce_taxinc = line.price_total / line.product_uom_qty if line.product_uom_qty else 0.0
-
-    @api.depends('price_subtotal', 'product_uom_qty')
-    def _get_price_reduce_notax(self):
-        for line in self:
-            line.price_reduce_taxexcl = line.price_subtotal / line.product_uom_qty if line.product_uom_qty else 0.0
-
-    @api.multi
-    def _compute_tax_id(self):
-        for line in self:
-            fpos = line.order_id.fiscal_position_id or line.order_id.partner_id.property_account_position_id
-            # If company_id is set, always filter taxes by the company
-            line_company_id = line.company_id or line.order_id.company_id
-            taxes = line.product_id.taxes_id.filtered(lambda r: not line_company_id or r.company_id == line_company_id)
-            line.tax_id = fpos.map_tax(taxes, line.product_id, line.order_id.partner_shipping_id) if fpos else taxes
-
-    @api.model
-    def _get_purchase_price(self, pricelist, product, product_uom, date):
-        return {}
-
-    @api.model
-    def _prepare_add_missing_fields(self, values):
-        """ Deduce missing required fields from the onchange """
-        res = {}
-        onchange_fields = ['name', 'rental_price', 'product_uom', 'tax_id']
-        if values.get('order_id') and values.get('product_id') and any(f not in values for f in onchange_fields):
-            line = self.new(values)
-            line.product_id_change()
-            for field in onchange_fields:
-                if field not in values:
-                    res[field] = line._fields[field].convert_to_write(line[field], line)
-        return res
 
     @api.model
     def create(self, values):
-        values.update(self._prepare_add_missing_fields(values))
         line = super(odoolocOrderLine, self).create(values)
-        if line.order_id.state == 'sale':
+        if line.order_id.state == 'rent':
             msg = _("Extra line with %s ") % (line.product_id.display_name,)
             line.order_id.message_post(body=msg)
-            # create an analytic account if at least an expense product
-            if line.product_id.expense_policy not in [False, 'no'] and not self.order_id.analytic_account_id:
-                self.order_id._create_analytic_account()
+
         return line
 
     @api.multi
@@ -326,7 +279,7 @@ class odoolocOrderLine(models.Model):
         if 'product_uom_qty' in values:
             precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
             self.filtered(
-                lambda r: r.state == 'sale' and float_compare(r.product_uom_qty, values['product_uom_qty'],
+                lambda r: r.state == 'rent' and float_compare(r.product_uom_qty, values['product_uom_qty'],
                                                               precision_digits=precision) != 0)._update_line_quantity(
                 values)
 
@@ -359,22 +312,18 @@ class odoolocOrderLine(models.Model):
     price_tax = fields.Float(compute='_compute_amount', string='Taxes', readonly=True, store=True)
     price_total = fields.Monetary(compute='_compute_amount', string='Total', readonly=True, store=True)
 
-    price_reduce = fields.Float(compute='_get_price_reduce', string='Price Reduce',
-                                digits=dp.get_precision('Product Price'), readonly=True, store=True)
     tax_id = fields.Many2many('account.tax', string='Taxes',
                               domain=['|', ('active', '=', False), ('active', '=', True)])
-    price_reduce_taxinc = fields.Monetary(compute='_get_price_reduce_tax', string='Price Reduce Tax inc', readonly=True,
-                                          store=True)
-    price_reduce_taxexcl = fields.Monetary(compute='_get_price_reduce_notax', string='Price Reduce Tax excl',
-                                           readonly=True, store=True)
 
     discount = fields.Float(string='Discount (%)', digits=dp.get_precision('Discount'), default=0.0)
 
-    product_id = fields.Many2one('product.product', string='Product', domain=[('rental', '=', True)],
+    product_id = fields.Many2one('product.template', string='Product', domain=[('rental', '=', True)],
                                  change_default=True, required=True)
 
-    product_updatable = fields.Boolean(compute='_compute_product_updatable', string='Can Edit Product', readonly=True, default=True)
-    product_uom_qty = fields.Float(string='Quantity', digits=dp.get_precision('Product Unit of Measure'), required=True, default=1.0)
+    product_updatable = fields.Boolean(compute='_compute_product_updatable', string='Can Edit Product', readonly=True,
+                                       default=True)
+    product_uom_qty = fields.Float(string='Quantity', digits=dp.get_precision('Product Unit of Measure'), required=True,
+                                   default=1.0)
     product_uom = fields.Many2one('product.uom', string='Unit of Measure', required=True)
 
     rentalsman_id = fields.Many2one(related='order_id.user_id', store=True, string='Rentalsperson', readonly=True)
@@ -385,212 +334,34 @@ class odoolocOrderLine(models.Model):
     state = fields.Selection([
         ('draft', 'Quotation'),
         ('sent', 'Quotation Sent'),
-        ('sale', 'Sales Order'),
+        ('rent', 'Rentals Order'),
         ('done', 'Done'),
         ('cancel', 'Cancelled'),
     ], related='order_id.state', string='Order Status', readonly=True, copy=False, store=True, default='draft')
 
     @api.multi
-    def _get_display_price(self, product):
-        # TO DO: move me in master/saas-16 on sale.order
-        if self.order_id.pricelist_id.discount_policy == 'with_discount':
-            return product.with_context(pricelist=self.order_id.pricelist_id.id).price
-        product_context = dict(self.env.context, partner_id=self.order_id.partner_id.id, date=self.order_id.date_order,
-                               uom=self.product_uom.id)
-        final_price, rule_id = self.order_id.pricelist_id.with_context(product_context).get_product_price_rule(
-            self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
-        base_price, currency_id = self.with_context(product_context)._get_real_price_currency(product, rule_id,
-                                                                                              self.product_uom_qty,
-                                                                                              self.product_uom,
-                                                                                              self.order_id.pricelist_id.id)
-        if currency_id != self.order_id.pricelist_id.currency_id.id:
-            base_price = self.env['res.currency'].browse(currency_id).with_context(product_context).compute(base_price,
-                                                                                                            self.order_id.pricelist_id.currency_id)
-        # negative discounts (= surcharge) are included in the display price
-        return max(base_price, final_price)
-
-    @api.multi
     @api.onchange('product_id')
-    def product_id_change(self):
-        if not self.product_id:
-            return {'domain': {'product_uom': []}}
+    def on_change_product_id(self):
+        if self.product_id.description_sale == "" or not self.product_id.description_sale or self.product_id.description_sale == None:
+            rol_desc = self.product_id.name
+        else:
+            rol_desc = self.product_id.description_sale
 
-        vals = {}
-        domain = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
-        if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
-            vals['product_uom'] = self.product_id.uom_id
-            vals['product_uom_qty'] = 1.0
+        res = {
+            'value': {
+                'name': rol_desc,
+                'quantity': '1.0',
+                'rental_price': self.product_id.rental_price,
+            }
+        }
 
-        product = self.product_id.with_context(
-            lang=self.order_id.partner_id.lang,
-            partner=self.order_id.partner_id.id,
-            quantity=vals.get('product_uom_qty') or self.product_uom_qty,
-            date=self.order_id.date_order,
-            pricelist=self.order_id.pricelist_id.id,
-            uom=self.product_uom.id
-        )
+        return res
 
-        result = {'domain': domain}
-
-        title = False
-        message = False
-        warning = {}
-        if product.sale_line_warn != 'no-message':
-            title = _("Warning for %s") % product.name
-            message = product.sale_line_warn_msg
-            warning['title'] = title
-            warning['message'] = message
-            result = {'warning': warning}
-            if product.sale_line_warn == 'block':
-                self.product_id = False
-                return result
-
-        name = product.name_get()[0][1]
-        if product.description_sale:
-            name += '\n' + product.description_sale
-        vals['name'] = name
-
-        self._compute_tax_id()
-
-        if self.order_id.pricelist_id and self.order_id.partner_id:
-            vals['rental_price'] = self.env['account.tax']._fix_tax_included_price_company(
-                self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
-        self.update(vals)
-
-        return result
-
-    @api.onchange('product_uom', 'product_uom_qty')
-    def product_uom_change(self):
-        if not self.product_uom or not self.product_id:
-            self.rental_price = 0.0
-            return
-        if self.order_id.pricelist_id and self.order_id.partner_id:
-            product = self.product_id.with_context(
-                lang=self.order_id.partner_id.lang,
-                partner=self.order_id.partner_id.id,
-                quantity=self.product_uom_qty,
-                date=self.order_id.date_order,
-                pricelist=self.order_id.pricelist_id.id,
-                uom=self.product_uom.id,
-                fiscal_position=self.env.context.get('fiscal_position')
-            )
-            self.rental_price = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product),
-                                                                                      product.taxes_id, self.tax_id,
-                                                                                      self.company_id)
-
-    @api.multi
-    def name_get(self):
-        result = []
-        for so_line in self:
-            name = '%s - %s' % (so_line.order_id.name, so_line.name.split('\n')[0] or so_line.product_id.name)
-            if so_line.order_partner_id.ref:
-                name = '%s (%s)' % (name, so_line.order_partner_id.ref)
-            result.append((so_line.id, name))
-        return result
-
-    @api.model
-    def name_search(self, name='', args=None, operator='ilike', limit=100):
-        if operator in ('ilike', 'like', '=', '=like', '=ilike'):
-            args = expression.AND([
-                args or [],
-                ['|', ('order_id.name', operator, name), ('name', operator, name)]
-            ])
-        return super(odoolocOrderLine, self).name_search(name, args, operator, limit)
+    # {'value': {'rental_price': self.product_id.rental_price}}
 
     @api.multi
     def unlink(self):
-        if self.filtered(lambda x: x.state in ('sale', 'done')):
+        if self.filtered(lambda x: x.state in ('rent', 'done')):
             raise UserError(
-                _('You can not remove a sales order line.\nDiscard changes and try setting the quantity to 0.'))
+                _('You can not remove a rentals order line.\nDiscard changes and try setting the quantity to 0.'))
         return super(odoolocOrderLine, self).unlink()
-
-    def _get_real_price_currency(self, product, rule_id, qty, uom, pricelist_id):
-        """Retrieve the price before applying the pricelist
-            :param obj product: object of current product record
-            :parem float qty: total quentity of product
-            :param tuple price_and_rule: tuple(price, suitable_rule) coming from pricelist computation
-            :param obj uom: unit of measure of current order line
-            :param integer pricelist_id: pricelist id of sales order"""
-        PricelistItem = self.env['product.pricelist.item']
-        field_name = 'lst_price'
-        currency_id = None
-        product_currency = None
-        if rule_id:
-            pricelist_item = PricelistItem.browse(rule_id)
-            if pricelist_item.pricelist_id.discount_policy == 'without_discount':
-                while pricelist_item.base == 'pricelist' and pricelist_item.base_pricelist_id and pricelist_item.base_pricelist_id.discount_policy == 'without_discount':
-                    price, rule_id = pricelist_item.base_pricelist_id.with_context(uom=uom.id).get_product_price_rule(
-                        product, qty, self.order_id.partner_id)
-                    pricelist_item = PricelistItem.browse(rule_id)
-
-            if pricelist_item.base == 'standard_price':
-                field_name = 'standard_price'
-            if pricelist_item.base == 'pricelist' and pricelist_item.base_pricelist_id:
-                field_name = 'price'
-                product = product.with_context(pricelist=pricelist_item.base_pricelist_id.id)
-                product_currency = pricelist_item.base_pricelist_id.currency_id
-            currency_id = pricelist_item.pricelist_id.currency_id
-
-        product_currency = product_currency or (
-                    product.company_id and product.company_id.currency_id) or self.env.user.company_id.currency_id
-        if not currency_id:
-            currency_id = product_currency
-            cur_factor = 1.0
-        else:
-            if currency_id.id == product_currency.id:
-                cur_factor = 1.0
-            else:
-                cur_factor = currency_id._get_conversion_rate(product_currency, currency_id)
-
-        product_uom = self.env.context.get('uom') or product.uom_id.id
-        if uom and uom.id != product_uom:
-            # the unit price is in a different uom
-            uom_factor = uom._compute_price(1.0, product.uom_id)
-        else:
-            uom_factor = 1.0
-
-        return product[field_name] * uom_factor * cur_factor, currency_id.id
-
-    def _get_protected_fields(self):
-        return [
-            'product_id', 'name', 'rental_price', 'product_uom', 'product_uom_qty',
-            'tax_id', 'analytic_tag_ids'
-        ]
-
-    @api.onchange('product_id', 'rental_price', 'product_uom', 'product_uom_qty', 'tax_id')
-    def _onchange_discount(self):
-        if not (self.product_id and self.product_uom and
-                self.order_id.partner_id and self.order_id.pricelist_id and
-                self.order_id.pricelist_id.discount_policy == 'without_discount' and
-                self.env.user.has_group('sale.group_discount_per_so_line')):
-            return
-
-        self.discount = 0.0
-        product = self.product_id.with_context(
-            lang=self.order_id.partner_id.lang,
-            partner=self.order_id.partner_id.id,
-            quantity=self.product_uom_qty,
-            date=self.order_id.date_order,
-            pricelist=self.order_id.pricelist_id.id,
-            uom=self.product_uom.id,
-            fiscal_position=self.env.context.get('fiscal_position')
-        )
-
-        product_context = dict(self.env.context, partner_id=self.order_id.partner_id.id, date=self.order_id.date_order,
-                               uom=self.product_uom.id)
-
-        price, rule_id = self.order_id.pricelist_id.with_context(product_context).get_product_price_rule(
-            self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
-        new_list_price, currency_id = self.with_context(product_context)._get_real_price_currency(product, rule_id,
-                                                                                                  self.product_uom_qty,
-                                                                                                  self.product_uom,
-                                                                                                  self.order_id.pricelist_id.id)
-
-        if new_list_price != 0:
-            if self.order_id.pricelist_id.currency_id.id != currency_id:
-                # we need new_list_price in the same currency as price, which is in the SO's pricelist's currency
-                new_list_price = self.env['res.currency'].browse(currency_id).with_context(product_context).compute(
-                    new_list_price, self.order_id.pricelist_id.currency_id)
-            discount = (new_list_price - price) / new_list_price * 100
-            if discount > 0:
-                self.discount = discount
--- addons/odooloc/models/product_template.py
+++ addons/odooloc/models/product_template.py
@@ -6,5 +6,5 @@ from odoo.addons import decimal_precision as dp
 # Adding product rental price for rentable products
 class ProductTemplate(models.Model):
     _inherit = "product.template"
-    rental_price_unit = fields.Float('Unit price per day', digits=dp.get_precision('Rental Price'),
+    rental_price = fields.Float('Unit price per day', digits=dp.get_precision('Rental Price'),
                                      domain=[('rental', '=', True)])
