1a93f26c52989f32a45d7549723c3c6ae0a2cf36
fheche <francis.heche@gmail.com>
2019-09-05 18:01:37 +0200

    add stock.move and stock.picking creation on rental confirmation

--- addons/odooloc_stock/models/odooloc_order.py
+++ addons/odooloc_stock/models/odooloc_order.py
@@ -25,7 +25,24 @@ class odoolocOrder(models.Model):
     picking_ids = fields.One2many('stock.picking', 'odooloc_id', string='Pickings')
 
 
+    @api.multi
+    def odooloc_confirm_order(self):
+        super(odoolocOrder, self).odooloc_confirm_order()
+        for order in self:
+            order.order_line._create_move()
 
+    @api.multi
+    def _create_picking(self):
+        stock_location = self.env.ref('stock.stock_location_stock')
+        self.picking_ids = self.env['stock.picking'].create({
+            'location_id': stock_location.id,
+            'location_dest_id': stock_location.id,
+            'picking_type_id': 4,
+            'move_type': self.picking_policy,
+        })
+
+        for line in self.order_line:
+            line.move_ids.picking_id =  self.picking_ids
 
 class odoolocOrderLine(models.Model):
     _inherit = 'odooloc.order.line'
@@ -33,10 +50,24 @@ class odoolocOrderLine(models.Model):
     move_ids = fields.One2many('stock.move', 'odooloc_line_id', string='Stock Moves')
 
     @api.multi
-    def _action_launch_procurement_rule(self):
+    def _create_move(self):
+        stock_location = self.env.ref('stock.stock_location_stock')
         for line in self:
-            values = line._prepare_procurement_values(group_id=group_id)
-            
-            self.env['procurement.group'].run(line.product_id, line.product_uom_qty, line.product_uom,
-                                              line.order_id.partner_id.property_stock_customer, line.name,
-                                              line.order_id.name, values)
+            self.move_ids = self.env['stock.move'].create({
+                'name': 'Items preparation for a rent',
+                'location_id': stock_location.id,
+                'location_dest_id': stock_location.id,
+                'product_id': line.product_id.id,
+                'product_uom': line.product_uom.id,
+                'product_uom_qty': line.product_uom_qty,
+                #'picking_id': self.order_id.
+            })
+            self.move_ids._action_confirm()
+            self.move_ids._action_assign()
+            # This creates a stock.move.line record.
+            # You could also do it manually using self.env['stock.move.line'].create({...})
+            #move_ids.move_line_ids.write({'qty_done': line.product_uom_qty})
+            self.move_ids._action_done()
+
+        self.order_id._create_picking()
+
