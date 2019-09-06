d4cb7618d78fe881058458f8185bbd4dffb7642d
fheche <francis.heche@gmail.com>
2019-09-06 19:48:16 +0200

    Add delivery button on rental order view

--- addons/odooloc_stock/models/odooloc_order.py
+++ addons/odooloc_stock/models/odooloc_order.py
@@ -24,6 +24,29 @@ class odoolocOrder(models.Model):
         default=_default_warehouse_id)
 
     picking_ids = fields.One2many('stock.picking', 'odooloc_id', string='Pickings')
+    delivery_count = fields.Integer(string='Delivery Orders', compute='_compute_picking_ids')
+
+    @api.depends('picking_ids')
+    def _compute_picking_ids(self):
+        for order in self:
+            order.delivery_count = len(order.picking_ids)
+
+    @api.multi
+    def action_view_delivery(self):
+        '''
+        This function returns an action that display existing delivery orders
+        of given sales order ids. It can either be a in a list or in a form
+        view, if there is only one delivery order to show.
+        '''
+        action = self.env.ref('stock.action_picking_tree_all').read()[0]
+
+        pickings = self.mapped('picking_ids')
+        if len(pickings) > 1:
+            action['domain'] = [('id', 'in', pickings.ids)]
+        elif pickings:
+            action['views'] = [(self.env.ref('stock.view_picking_form').id, 'form')]
+            action['res_id'] = pickings.id
+        return action
 
     @api.multi
     def odooloc_confirm_order(self):
